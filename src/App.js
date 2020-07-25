5bc5df105a7ce4443a8c0ca29d99eb5fb9f86a74
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-07-25 07:35:30 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://res.cloudinary.com/codelifings/image/upload/v1590743307/Tak_berjudul8_hozcsn.png" className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>      
+      </div>
+    );
+  }
+}
+
+export default App;
